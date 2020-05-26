0624b97e6b3481a38eb1ce0c7f16c75e4554ea76
pernillehm <56149661+pernillehm@users.noreply.github.com>
2020-05-26 18:39:35 +0200

    Add files via upload

---
+++ js/forhandler-accordion.js
@@ -0,0 +1,18 @@
+"use strict";
+// kode fra: https://www.w3schools.com/howto/howto_js_accordion.asp
+let acc = document.getElementsByClassName("accordion-forhandler");
+let i;
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+        /* Toggle between adding and removing the "active" class,
+        to highlight the button that controls the panel */
+        this.classList.toggle("activate");
+        /* Toggle between hiding and showing the active panel */
+        let panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    });
+}
---
+++ js/loading.js
@@ -0,0 +1,18 @@
+// kode fra: https://codepen.io/ManalHamad/pen/jdryWL
+		
+
+jQuery(document).ready(function() {
+			jQuery(".bar-color").css({
+				"background": "#F9C553",
+				"width": "60%"
+			});
+			jQuery(".skills:nth-of-type(2) .bar-color").css({
+				"width": "80%"
+			});
+			jQuery(".skills:nth-of-type(3) .bar-color").css({
+				"width": "70%"
+			});
+			jQuery(".skills:nth-of-type(4) .bar-color").css({
+				"width": "55%"
+			});
+		});
